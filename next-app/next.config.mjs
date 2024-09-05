91ea67fbd7ceb2c2b2421359854cdd34d252b149
Asel <asel.alimbaeva.kk@gmail.com>
2024-09-05 09:41:20 +0300

    Configure GitHub Pages deployment

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // отключает оптимизацию изображений, так как GitHub Pages не поддерживает серверную обработку изображений
+  },
+  basePath: '/REPO_NAME', // замени REPO_NAME на название твоего репозитория
+  assetPrefix: '/REPO_NAME/',
+};
 
 export default nextConfig;
